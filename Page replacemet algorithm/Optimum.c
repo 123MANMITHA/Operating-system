53af1ede2da63b26edb3bb70fdec812e05542dec
MANMITHA <120546462+123MANMITHA@users.noreply.github.com>
2023-07-25 10:56:08 +0000

    commit

---
+++ Page replacemet algorithm/Optimum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdbool.h>
+int findIndex(int frames[],int n,int page)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(frames[i]==page)
+        return i;
+    }
+    return -1;
+}
+void printFrames(int frames[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(frames[i]==-1)
+        printf("-");
+        else
+        printf("%d",frames[i])
+    }
+}
