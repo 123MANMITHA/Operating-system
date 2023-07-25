06b28a5f2999c88e69981c84c097b93a13337d5e
MANMITHA <120546462+123MANMITHA@users.noreply.github.com>
2023-07-25 10:06:03 +0000

    commit

---
+++ Page replacemet algorithm/FIFO.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,a[50],frame[10],no,k,avail,fault=0;
+    printf("\nEnter the numberb of pages:\n");
+    scanf("%d",&n);
+    printf("\nEnter the page number:\n");
+    for(i=1;i<=n;i++)
+    scanf("%d",&a[i]);
+    printf("\nEnter the number of frames:");
+    scanf("%d",&no);
+    for(i=0;i<no;i++)
+        frame[i]=-1;
+        j=0;
+        printf("\tinput\tpage frames\n");
+        for(i=1;i<=n;i++)
+        {
+            printf("%d\t\t",a[i]);
+            avail=0;
+            for(k=0;k<no;k++)
+            if(frame[k]==a[i])
+            avail=1;
+            if(avail==0)
+            {
+                frame[j]=a[i];
+                fault++;
+                for(k=0;k<no;k++)
+                printf("%d\t",frame[k]);
+                j=(j+1)%no;
+            }
+            printf("\n");
+        }
+            printf("Page Faults is %d",fault);
+            return 0;
+
+}
+        
+
+    
+        
+    
+
